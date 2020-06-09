31c01d87c146ce660a2bc74b7b74ffc6520286e4
annyup <x-annypham@live.com>
2020-06-09 00:46:19 -0400

    fixed styling, media queries and index

--- script.js
+++ script.js
@@ -3,129 +3,129 @@
 const btsSongArray = [
   {
     title: 'No More Dream',
-    img: 'assets/album1.jpg',
+    img: 'assets/song1.jpg',
     url: 'assets/btsSongs/sound1.mp3',
-    alt: '2 Cool 4 Skool Album Cover'
+    alt: '2 Cool 4 Skool Concept Photo'
   },
   {
-    title: 'N.O',
-    img: 'assets/album2.jpg',
+    title: 'N.O.',
+    img: 'assets/song2.jpg',
     url: 'assets/btsSongs/sound2.mp3',
-    alt: 'O!RUL8,2? Album Cover'
+    alt: 'O!RUL8,2? Concept Photo'
   },
   {
     title: 'Boy in Luv',
-    img: 'assets/album3.jpg',
+    img: 'assets/song3.jpg',
     url: 'assets/btsSongs/sound3.mp3',
-    alt: 'Skool Luv Affair Album Cover'
+    alt: 'Skool Luv Affair Concept Photo'
   },
   {
     title: 'Danger',
-    img: 'assets/album4.jpg',
+    img: 'assets/song4.jpg',
     url: 'assets/btsSongs/sound4.mp3',
-    alt: 'Dark & Wild Album Cover'
+    alt: 'Dark & Wild Concept Photo'
   },
   {
     title: 'War of Hormones',
-    img: 'assets/album4.jpg',
+    img: 'assets/song5.jpg',
     url: 'assets/btsSongs/sound5.mp3',
-    alt: 'Dark & Wild Album Cover'
+    alt: 'Dark & Wild Concept Photo'
   },
   {
     title: 'I need U',
-    img: 'assets/album5.jpg',
+    img: 'assets/song6.jpg',
     url: 'assets/btsSongs/sound6.mp3',
-    alt: 'The Most Beautiful Moment in Life Part 1 Album Cover'
+    alt: 'The Most Beautiful Moment in Life Part 1 Concept Photo'
   },
   {
     title: 'Dope',
-    img: 'assets/album5.jpg',
+    img: 'assets/song7.jpg',
     url: 'assets/btsSongs/sound7.mp3',
-    alt: 'The Most Beautiful Moment in Life Part 1 Album Cover'
+    alt: 'The Most Beautiful Moment in Life Part 1 Concept Photo'
   },
   {
     title: 'Run',
-    img: 'assets/album6.jpg',
+    img: 'assets/song8.jpg',
     url: 'assets/btsSongs/sound8.mp3',
-    alt: 'The Most Beautiful Moment in Life Part 2 Album Cover'
+    alt: 'The Most Beautiful Moment in Life Part 2 Concept Photo'
   },
   {
     title: 'Fire',
-    img: 'assets/album7.jpg',
+    img: 'assets/song9.jpg',
     url: 'assets/btsSongs/sound9.mp3',
-    alt: 'Young Forever Album Cover'
+    alt: 'Young Forever Concept Photo'
   },
   {
     title: 'Save Me',
-    img: 'assets/album7.jpg',
+    img: 'assets/song10.jpg',
     url: 'assets/btsSongs/sound10.mp3',
-    alt: 'Young Forever Album Cover'
+    alt: 'Young Forever Concept Photo'
   },
   {
     title: 'Blood Sweat & Tears',
-    img: 'assets/album8.jpg',
+    img: 'assets/song11.jpg',
     url: 'assets/btsSongs/sound11.mp3',
-    alt: 'Wings Album Cover'
+    alt: 'Wings Concept Photo'
   },
   {
     title: 'Spring Day',
-    img: 'assets/album9.jpg',
+    img: 'assets/song12.jpg',
     url: 'assets/btsSongs/sound12.mp3',
-    alt: 'You Never Walk Alone Album Cover'
+    alt: 'You Never Walk Alone Concept Photo'
   },
   {
     title: 'Not Today',
-    img: 'assets/album9.jpg',
+    img: 'assets/song13.jpg',
     url: 'assets/btsSongs/sound13.mp3',
-    alt: 'You Never Walk Alone Album Cover'
+    alt: 'You Never Walk Alone Concept Photo'
   },
   {
     title: 'DNA',
-    img: 'assets/album10.jpg',
+    img: 'assets/song14.jpg',
     url: 'assets/btsSongs/sound14.mp3',
-    alt: 'Love Yourself: Her Album Cover'
+    alt: 'Love Yourself: Her Concept Photo'
   },
   {
     title: 'Mic Drop',
-    img: 'assets/album10.jpg',
+    img: 'assets/song15.jpg',
     url: 'assets/btsSongs/sound15.mp3',
-    alt: 'Love Yourself: Her Album Cover'
+    alt: 'Love Yourself: Her Concept Photo'
   },
   {
     title: 'Fake Love',
-    img: 'assets/album11.jpg',
+    img: 'assets/song16.jpg',
     url: 'assets/btsSongs/sound16.mp3',
-    alt: 'Love Yourself: Tear Album Cover'
+    alt: 'Love Yourself: Tear Concept Photo'
   },
   {
     title: 'IDOL',
-    img: 'assets/album12.jpg',
+    img: 'assets/song17.jpg',
     url: 'assets/btsSongs/sound17.mp3',
-    alt: 'Love Yourself: Answer Album Cover'
+    alt: 'Love Yourself: Answer Concept Photo'
   },
   {
     title: 'Waste it on me',
-    img: 'assets/album13.jpg',
+    img: 'assets/song18.jpg',
     url: 'assets/btsSongs/sound18.mp3',
-    alt: 'Waste it on me Cover image'
+    alt: 'BTS featuring Steve Aoki'
   },
   {
     title: 'Boy with Luv',
-    img: 'assets/album14.jpg',
+    img: 'assets/song19.jpg',
     url: 'assets/btsSongs/sound19.mp3',
-    alt: 'Map of the Soul: Persona Album Cover'
+    alt: 'Map of the Soul: Persona Concept Photo'
   },
   {
     title: 'Black Swan',
-    img: 'assets/album15.jpg',
+    img: 'assets/song20.jpg',
     url: 'assets/btsSongs/sound20.mp3',
-    alt: 'Map of the Soul: 7 Album Cover'
+    alt: 'Map of the Soul: 7 Concept Photo'
   },
   {
     title: 'ON',
-    img: 'assets/album16.jpg',
+    img: 'assets/song21.jpg',
     url: 'assets/btsSongs/sound21.mp3',
-    alt: 'Map of the Soul: 7 Album Cover'
+    alt: 'Map of the Soul: 7 Concept Photo'
   }
 ]
 
@@ -155,13 +155,10 @@ $(function() {
       songContainer.append(songImage, songTitle, songUrl);
 
       $(`#song${songCount++}`).append(songContainer);
-
     })
-    
   }
 
   playSong(btsSongArray);
-
 
   //  Runs a function when mouse enters on the button
   $(".albumButton > span").on("mouseenter", function(hoverEvent){
@@ -171,14 +168,11 @@ $(function() {
 
     // Adds a class of active on current hovered button
     $(this).parent().addClass("active");
-
   });
-
 });
 
 // Play music function
 // Music will play depending on which button the user has clicked
-
 const play = function playMusicById (soundId) {
 
   let audio = document.getElementById(soundId);
@@ -255,15 +249,8 @@ const randomPlay = function () {
       $('#randomize span').text('randomize');
       $('#randomize').val ('stopped');
     }
-
   });
 }
 
 // Calls the function when button is clicked
 randomPlay();
-
-
-// If user were to click button more than once, it will ask user if it would like to listen to more
-// If user clicks yes, links to site
-// Else, exit
-
